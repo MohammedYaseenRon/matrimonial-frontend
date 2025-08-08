83e7d2f1eea2094b9d31e4135dcb38d5b941778c
Mohammed Usmani <mohammedusmani2005@gmail.com>
2025-08-08 14:47:21 +0530

    feature: API proxying

--- frontend/constants/Configs.ts
+++ frontend/constants/Configs.ts
@@ -1,4 +1,7 @@
-const SERVER_URL = process.env.EXPO_PUBLIC_SERVER_BASE_URL || 'http://localhost:3000';
+const SERVER_URL = process.env.NODE_ENV === 'production' 
+  ? '/api'
+  : process.env.EXPO_PUBLIC_SERVER_BASE_URL || 'http://localhost:3000';
+
 export const Configs = {
   SERVER_URL,
   // Add other configurations as needed
